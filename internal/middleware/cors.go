d5b758391556b2de571ed1fd1e5667fc2e71e0cb
HardDie <oleg1995sysoev@yandex.ru>
2023-04-22 19:42:00 +0300

    Fixed CORS for editor.swagger.io

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -5,9 +5,12 @@ import "net/http"
 // CorsMiddleware CORS Headers middleware
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Origin", "https://editor.swagger.io")
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PATCH,UPDATE,DELETE,OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token, Authorization")
+		if (*r).Method == http.MethodOptions {
+			return
+		}
 		next.ServeHTTP(w, r)
 	})
 }
